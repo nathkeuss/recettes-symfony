0ce4a9de018da6cc668680b135cdb648c995369e
keuss le mojo <julio.nathan47@gmail.com>
2024-12-11 14:15:39 +0100

    liste recettes

--- src/Controller/Admin/AdminRecipeController.php
+++ src/Controller/Admin/AdminRecipeController.php
@@ -4,15 +4,16 @@ namespace App\Controller\Admin;
 
 use App\Entity\Recipe;
 use App\Form\AdminRecipeType;
+use App\Repository\RecipeRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use http\Env\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdminRecipeController extends AbstractController
 {
 
-    #[Route('admin/recipe/create', 'admin_create_recipe', methods: ['GET, POST'])]
+    #[Route('admin/recipe/create', 'admin_create_recipe', methods: ['GET', 'POST'])]
     public function createRecipe(Request $request, EntityManagerInterface $entityManager)
     {
         //nouvelle instance de l'entité Recipe, qui sera utilisée pour construire le formulaire
@@ -31,6 +32,8 @@ class AdminRecipeController extends AbstractController
             $entityManager->persist($recipe);
             // enregistre les changements dans la bdd
             $entityManager->flush();
+
+            $this->addFlash('success', 'Recette créée!');
         }
 
         //générer la vue du formulaire dans la template twig
@@ -40,6 +43,18 @@ class AdminRecipeController extends AbstractController
         //adminRecipeFormView qui contient la vue du formulaire
         return $this->render('admin/recipe/create_recipe.html.twig', [
             'adminRecipeFormView' => $adminRecipeFormView,
+        ]);
+    }
+
+    #[Route('admin/recipes/list', 'admin_list_recipes', methods: ['GET'])]
+    public function listRecipes(RecipeRepository $recipeRepository)
+    {
+        //récupère toutes les recettes
+        $recipes = $recipeRepository->findAll();
+
+        //rend la vue avec la liste des recettes
+        return $this->render('admin/recipe/list_recipes.html.twig', [
+            'recipes' => $recipes,
         ]);
     }
 }
